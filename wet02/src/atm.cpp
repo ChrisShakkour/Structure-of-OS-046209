46f2998c028a3633c4f3b23b8409c10425798aea
chris <chrisshakkour@gmail.com>
2022-05-13 15:21:08 +0300

    adds initial shell of source codes, adds account and atm classes

---
+++ wet02/src/atm.cpp
@@ -0,0 +1,98 @@
+#include "atm.h"
+
+#define ATM_SLEEP 100000
+
+//extern ofstream output_log;
+
+
+/* description */
+void atm::wrong_password_check_and_print (int id, int account_password, int inserted_password)
+{
+	
+}
+
+/* description */
+void atm::erase_account_by_id(int local_account_num)
+{
+	
+}
+
+/* description */
+account* atm::find_account(int inserted_acc_num, int* is_found)
+{
+	
+}
+
+/* description */
+void atm::error_print(char letter, int inserted_acc_num, int target_account, int is_found)
+{
+	
+}
+
+/* description */
+bool atm::init_atm_func(void* atm_inst)
+{
+	
+}
+
+/* description */
+void atm::all_functions_caller()
+{
+	
+}
+
+/* description */
+void atm::O_function(int inserted_acc_num, int inserted_password, int inserted_balance)
+{
+	
+}
+
+/* description */
+void atm::D_function(int inserted_password, int inserted_amount, account* account_ptr)
+{
+	
+}
+
+/* description */
+void atm::W_function(int inserted_password, int inserted_amount, account* account_ptr)
+{
+	
+}
+
+/* description */
+void atm::B_function(int inserted_password, account* account_ptr)
+{
+	
+}
+
+/* description */
+void atm::Q_function(int inserted_password, account* account_ptr)
+{
+	
+}
+
+/* description */
+void atm::T_function(int inserted_password_src, int inserted_amount, account* account_src_ptr, account* account_dest_ptr)
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ wet02/src/bank.h
@@ -0,0 +1,13 @@
+#ifndef BANK_H
+#define BANK_H
+
+
+#include "atm.h"
+#include "account.h"
+
+
+int main(int argc, char* argv[]);
+
+
+
+#endif
